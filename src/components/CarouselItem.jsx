3e7f56493e1a000e8386f519197d1270e017e744
Manuel-aguilera <Manueleitor.information@gmail.com>
2020-10-14 17:26:01 -0500

    3.0 PropTypes

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CorouselItem = ({ cover, title, year, contentRating, duration }) => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className='carousel-item'>
     <img className='carousel-item__img' src={cover} alt={title} />
     <div className='carousel-item__details'>
@@ -27,4 +28,12 @@ const CorouselItem = ({ cover, title, year, contentRating, duration }) => (
   </div>
 );
 
-export default CorouselItem;
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
+
+export default CarouselItem;
